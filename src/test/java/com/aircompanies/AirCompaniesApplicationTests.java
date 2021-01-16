6d7f9806706e32c7a2e42156ddebfcef9d6c3904
VitaliyDzen <vitaliy.dzen.303@gmail.com>
2021-01-16 17:06:25 +0200

    Create skeleton of project.

---
+++ src/test/java/com/aircompanies/AirCompaniesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aircompanies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirCompaniesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
