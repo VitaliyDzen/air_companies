a2d3cadb1c56022a3198fa57101edc3a87a0821b
VitaliyDzen <vitaliy.dzen.303@gmail.com>
2021-01-16 17:16:27 +0200

    Create ModelMapperConfig and constants for database and resource mapping

---
+++ src/main/java/com/aircompanies/constants/DBConstant.java
@@ -0,0 +1,8 @@
+package com.aircompanies.constants;
+
+public final class DBConstant {
+    public static final String AIRPLANE_TABLE = "airplane";
+    public static final String AIR_COMPANY_TABLE = "air_company";
+    public static final String FLIGHT_TABLE = "flight";
+
+}
---
+++ src/main/java/com/aircompanies/constants/ResourceMapping.java
@@ -0,0 +1,8 @@
+package com.aircompanies.constants;
+
+public final class ResourceMapping {
+    public static final String AIRPLANE = "airplane";
+    public static final String AIR_COMPANY = "air-company";
+    public static final String FLIGHT = "flight";
+
+}
