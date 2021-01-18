41c0fdb3bba85099b3752a31cd598a527f320fbe
VitaliyDzen <vitaliy.dzen.303@gmail.com>
2021-01-18 16:46:40 +0200

    Updater constants ErrorMessage and insertData.sql script

--- src/main/java/com/aircompanies/constants/ErrorMessage.java
+++ src/main/java/com/aircompanies/constants/ErrorMessage.java
@@ -6,4 +6,9 @@ public final class ErrorMessage {
     public static final String AIR_COMPANY_NOT_UPDATED_WITH_ID = "Air company not updated with id ";
     public static final String AIR_COMPANY_NOT_DELETED_WITH_ID = "Air company not deleted with id ";
     public static final String THIS_FIELD_CAN_T_BE_NULL = "This field can't be null";
+    public static final String AIRPLANE_NOT_UPDATED_WITH_ID = "Airplane not updated with id ";
+    public static final String AIRPLANE_NOT_FOUND_WITH_ID = "Airplane not found with id ";
+    public static final String FLIGHT_NOT_UPDATED_WITH_ID = "Flight not updated with id ";
+    public static final String FLIGHT_NOT_FOUND_WITH_ID = "Flight not found with id ";
+    public static final String CANT_SET_FLIGHT_STATUS_PENDING = "Can't set flight status PENDING";
 }
