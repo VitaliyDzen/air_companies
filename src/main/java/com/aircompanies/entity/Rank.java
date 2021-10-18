2867ded131858d695550423fcbe5b8288b9bd760
qqqLeo <44520458+qqqLeo@users.noreply.github.com>
2021-10-18 22:06:05 +0300

    Create Rank.java
    Created Rank

---
+++ src/main/java/com/aircompanies/entity/Rank.java
@@ -0,0 +1,40 @@
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = RANK)
+public class Rank {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 100, nullable = false, unique = true)
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "rank", fetch = FetchType.LAZY)
+    private List<Teacher> teachers;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
